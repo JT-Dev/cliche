8c604df4d02c4f3b3119681f903b24c58cb4355c
james-ridey <james-ridey@JAMESLAPTOP>
2013-05-18 18:40:49 +1000

    Import game libs and template

---
+++ the-game/src/com/jtdev/thegame/Main.java
@@ -0,0 +1,40 @@
+package com.jtdev.thegame;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class Main extends BasicGame
+{
+    public Main()
+    {
+	super(Constants.GAME_NAME);
+    }
+
+    @Override
+    public void init(GameContainer gc) throws SlickException
+    {
+
+    }
+
+    @Override
+    public void update(GameContainer gc, int delta) throws SlickException
+    {
+
+    }
+
+    public void render(GameContainer gc, Graphics g) throws SlickException
+    {
+
+    }
+
+    public static void main(String[] args) throws SlickException
+    {
+	AppGameContainer app = new AppGameContainer(new Main());
+
+	app.setDisplayMode(800, 600, false);
+	app.start();
+    }
+}
